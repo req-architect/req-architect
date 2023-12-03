64d4d787e7a3226402548a9b9f5b18db7d7852f9
Marcin <mrcin.wawrzyniakk@gmail.com>
2023-12-03 14:56:57 +0100

    main page header finished refactoring of main page header

--- frontend/src/components/MainPageHeader.tsx
+++ frontend/src/components/MainPageHeader.tsx
@@ -5,13 +5,13 @@ export default function MainPageHeader() {
   return (
     <AppBar
       position="static"
-      sx={{ backgroundColor: '#EEEEEE', border: '1px solid green', boxShadow: 'none' }}>
+      sx={{ backgroundColor: '#EEEEEE', border: '1px solid green', boxShadow: 'none'}}>
       <Toolbar>
         <Typography
           variant="h5"
           color="black"
           component="div"
-          sx={{minWidth: "300px"}}>
+          sx={{minWidth: 'fit-content', mr: 2}}>
           PZSP2-KUKIWAKO
         </Typography>
         <Box
@@ -20,82 +20,25 @@ export default function MainPageHeader() {
           alignItems="center"
           sx={{flexGrow: 1}}>
             <TextField
-            id="outlined-basic"
-            label="Comment"
-            multiline
-            variant="outlined"
-            maxRows={1}
-            sx={{ width: "50%", mr: 2, bgcolor: "white", marginRight: 20}}>
-        </TextField>
+              id="outlined-basic"
+              label="Comment"
+              multiline
+              variant="outlined"
+              maxRows={1}
+              sx={{ width: "50%", mr: 2, bgcolor: "white"}}>
+            </TextField>
+            <Button
+              size="large"
+              sx={{ background: 'green', color: 'white', minWidth: "80px", height: "40px" , mr: 2}}>
+              SAVE
+            </Button>
         </Box>
         <Button
           color="inherit"
-          size="large"
-          sx={{ border: '1px solid green', color: 'green'}}>
+          sx={{ border: '1px solid green', color: 'green', minWidth: "150px", height: "40px" , mr: 2}}>
           Log out
         </Button>
       </Toolbar>
     </AppBar>
   );
 }
-
-// export default function MainPageHeader() {
-//   return (
-//     <Grid
-//       container
-//       sx={{
-//         height: "10vh",
-//         minWidth: 1025,
-//         visibility: "hidden",
-//       }}
-//     >
-//       <Grid
-//         container
-//         p={2}
-//         direction={"row"}
-//         justifyContent={"space-between"}
-//         alignItems="center"
-//         borderBottom="1px solid green"
-//         sx={{ visibility: "visible" }}
-//         bgcolor={"#EEEEEE"}
-//       >
-//         <Typography variant="h5" color="black">
-//           PZSP2-KUKIWAKO
-//         </Typography>
-
-//         <Grid
-//           container
-//           width="fit-content"
-//           height="fit-content"
-//           alignItems="center"
-//         >
-          // <TextField
-          //   id="outlined-basic"
-          //   label="Comment"
-          //   multiline
-          //   variant="outlined"
-          //   maxRows={1}
-          //   sx={{ width: "50vh", mr: 2, bgcolor: "white" }}
-          // />
-//           <Button
-//             size="medium"
-//             variant="contained"
-//             color="success"
-//             sx={{ height: "50%" }}
-//           >
-//             SAVE
-//           </Button>
-//         </Grid>
-
-//         <Button
-//           size="medium"
-//           variant="outlined"
-//           color="success"
-//           sx={{ height: "50%" }}
-//         >
-//           LOG OUT
-//         </Button>
-//       </Grid>
-//     </Grid>
-//   );
-// }
