614889a7fcc01bbaff27266049e611775e1e758f
Marcin <mrcin.wawrzyniakk@gmail.com>
2023-12-03 14:29:06 +0100

    main page header corrected label and text field layout

--- frontend/src/components/MainPageHeader.tsx
+++ frontend/src/components/MainPageHeader.tsx
@@ -1,65 +1,101 @@
-import Grid from "@mui/material/Grid";
-import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
+import {TextField, Button, Typography, AppBar, Toolbar, Box} from "@mui/material"
+
 
 export default function MainPageHeader() {
   return (
-    <Grid
-      container
-      sx={{
-        height: "10vh",
-        minWidth: 1025,
-        visibility: "hidden",
-      }}
-    >
-      <Grid
-        container
-        p={2}
-        direction={"row"}
-        justifyContent={"space-between"}
-        alignItems="center"
-        borderBottom="1px solid green"
-        sx={{ visibility: "visible" }}
-        bgcolor={"#EEEEEE"}
-      >
-        <Typography variant="h5" color="black">
+    <AppBar
+      position="static"
+      sx={{ backgroundColor: '#EEEEEE', border: '1px solid green', boxShadow: 'none' }}>
+      <Toolbar>
+        <Typography
+          variant="h5"
+          color="black"
+          component="div"
+          sx={{minWidth: "300px"}}>
           PZSP2-KUKIWAKO
         </Typography>
-
-        <Grid
-          container
-          width="fit-content"
-          height="fit-content"
+        <Box
+          display="flex"
+          justifyContent="center"
           alignItems="center"
-        >
-          <TextField
+          sx={{flexGrow: 1}}>
+            <TextField
             id="outlined-basic"
             label="Comment"
             multiline
             variant="outlined"
             maxRows={1}
-            sx={{ width: "50vh", mr: 2, bgcolor: "white" }}
-          />
-          <Button
-            size="medium"
-            variant="contained"
-            color="success"
-            sx={{ height: "50%" }}
-          >
-            SAVE
-          </Button>
-        </Grid>
-
+            sx={{ width: "50%", mr: 2, bgcolor: "white", marginRight: 20}}>
+        </TextField>
+        </Box>
         <Button
-          size="medium"
-          variant="outlined"
-          color="success"
-          sx={{ height: "50%" }}
-        >
-          LOG OUT
+          color="inherit"
+          size="large"
+          sx={{ border: '1px solid green', color: 'green'}}>
+          Log out
         </Button>
-      </Grid>
-    </Grid>
+      </Toolbar>
+    </AppBar>
   );
 }
+
+// export default function MainPageHeader() {
+//   return (
+//     <Grid
+//       container
+//       sx={{
+//         height: "10vh",
+//         minWidth: 1025,
+//         visibility: "hidden",
+//       }}
+//     >
+//       <Grid
+//         container
+//         p={2}
+//         direction={"row"}
+//         justifyContent={"space-between"}
+//         alignItems="center"
+//         borderBottom="1px solid green"
+//         sx={{ visibility: "visible" }}
+//         bgcolor={"#EEEEEE"}
+//       >
+//         <Typography variant="h5" color="black">
+//           PZSP2-KUKIWAKO
+//         </Typography>
+
+//         <Grid
+//           container
+//           width="fit-content"
+//           height="fit-content"
+//           alignItems="center"
+//         >
+          // <TextField
+          //   id="outlined-basic"
+          //   label="Comment"
+          //   multiline
+          //   variant="outlined"
+          //   maxRows={1}
+          //   sx={{ width: "50vh", mr: 2, bgcolor: "white" }}
+          // />
+//           <Button
+//             size="medium"
+//             variant="contained"
+//             color="success"
+//             sx={{ height: "50%" }}
+//           >
+//             SAVE
+//           </Button>
+//         </Grid>
+
+//         <Button
+//           size="medium"
+//           variant="outlined"
+//           color="success"
+//           sx={{ height: "50%" }}
+//         >
+//           LOG OUT
+//         </Button>
+//       </Grid>
+//     </Grid>
+//   );
+// }
