52993259a70c4a865f0c1fbcd283d77e52dc4747
mkielbus <mateusz.kielbus.mk@gmail.com>
2023-12-09 19:10:34 +0100

    Reversed last changes

--- backend/src/MyServer/views.py
+++ backend/src/MyServer/views.py
@@ -34,21 +34,21 @@ class ReqView(APIView):
     def _deleteRequirement(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to delete requirement. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.deleteUserRequirement(request.DELETE.get("docId"), request.DELETE.get("reqId"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.deleteUserRequirement(request.data.get("docId"), request.data.get("reqId"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to delete requirement. Specified requirement does not exist or could not build document tree'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
     def _editRequirement(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to modify requirement. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.editUserRequirement(request.PUT.get("docId"), request.PUT.get("reqId"), request.PUT.get("reqText"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.editUserRequirement(request.data.get("docId"), request.data.get("reqId"), request.data.get("reqText"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to modify requirement. At least one of specified uids is invalid or could not build document tree'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
     def _addRequirement(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to add requirement. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.addUserRequirement(request.POST.get("docId"), request.POST.get("reqNumberId"), request.POST.get("reqText"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.addUserRequirement(request.data.get("docId"), request.data.get("reqNumberId"), request.data.get("reqText"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to add requirement. Invalid document uid or invalid req number or could not build document tree.'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
@@ -84,14 +84,14 @@ class DocView(APIView):
     def _addDocument(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to add document. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.addUserDocument(request.POST.get("docId"), request.POST.get("parentId"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.addUserDocument(request.data.get("docId"), request.data.get("parentId"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to add document. Could not build documents tree or root document exists and you need to specify the parent document.'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
     def _deleteDocument(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to delete document. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.deleteUserDocument(request.DELETE.get("docId"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.deleteUserDocument(request.data.get("docId"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to delete document. Specified document does not exist or could not build document tree'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
@@ -118,7 +118,7 @@ class LinkView(APIView):
     def _addLink(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to link requirements. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.addUserLink(request.PUT.get("req1Id"), request.PUT.get("req2Id"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.addUserLink(request.data.get("req1Id"), request.data.get("req2Id"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to link requirements. At least one invalid requirement id or could not build document tree.'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
@@ -138,7 +138,7 @@ class UnlinkView(APIView):
     def _removeLink(self, request):
         if not self._serverInfo:
             return Response({'message': 'Unable to unlink requirements. Server configuration problem'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
-        if not MyServer.restHandlersHelpers.deleteUserLink(request.PUT.get("req1Id"), request.PUT.get("req2Id"), self._serverInfo["usersFolder"] + "/user"):
+        if not MyServer.restHandlersHelpers.deleteUserLink(request.data.get("req1Id"), request.data.get("req2Id"), self._serverInfo["usersFolder"] + "/user"):
             return Response({'message': 'Unable to unlink requirements. At least one invalid requirement id or could not build document tree.'}, status=status.HTTP_503_SERVICE_UNAVAILABLE)
         return Response({'message': 'OK'}, status=status.HTTP_200_OK)
 
