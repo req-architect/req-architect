7b76de241e2a2dc3aa7c4469b9d00d5155b65b37
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-12-10 04:36:25 +0100

    Selecting requirement on click

--- frontend/src/components/main/requirement-list/RequirementComponent.tsx
+++ frontend/src/components/main/requirement-list/RequirementComponent.tsx
@@ -1,9 +1,10 @@
 import { Requirement } from "../../../types";
 import { Box } from "@mui/material";
 import RequirementEditMode from "./RequirementEditMode.tsx";
-import { useContext } from "react";
+import { useContext, useRef } from "react";
 import { MainContext } from "../../../pages/MainPage.tsx";
 import RequirementStandard from "./RequirementStandard.tsx";
+import useClickInside from "../../../hooks/useClickInside.ts";
 
 export default function RequirementComponent({
     requirement,
@@ -11,16 +12,19 @@ export default function RequirementComponent({
     requirement: Requirement;
 }) {
     const contextTools = useContext(MainContext);
+    const wrapperRef = useRef<HTMLInputElement>(null);
     function handleSelect() {
         contextTools?.updateSelectedRequirement(requirement);
     }
+    useClickInside(wrapperRef, handleSelect);
     return (
         // add an outline to box if it is selected
         // TODO: change width of the box if selected && edit mode
         <Box
+            ref={wrapperRef}
             sx={
                 contextTools?.isSelected(requirement)
-                    ? { border: "2px solid green", marginBottom: 4 }
+                    ? { outline: "2px solid green", marginBottom: 4 }
                     : { marginBottom: 4 }
             }
         >
