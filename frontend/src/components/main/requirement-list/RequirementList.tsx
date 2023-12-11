28cfab1e8405dda1601bb310550e234f832a7190
Marcin <mrcin.wawrzyniakk@gmail.com>
2023-12-11 01:49:45 +0100

    document list when a document has no requirement, the requirement list is replaced wiht an informing typography

--- frontend/src/components/main/requirement-list/RequirementList.tsx
+++ frontend/src/components/main/requirement-list/RequirementList.tsx
@@ -1,6 +1,6 @@
 import { Requirement } from "../../../types.ts";
 import RequirementComponent from "./RequirementComponent.tsx";
-import { List, Box } from "@mui/material";
+import { List, Box, Typography } from "@mui/material";
 
 export default function RequirementList({
     requirements,
@@ -19,6 +19,7 @@ export default function RequirementList({
     //     { id: "7", reviewed: false, text: "System shall provide feature 7" },
     // ]);
     return (
+        requirements.length > 0 ?
         <List>
             {requirements.map((requirement) => (
                 <Box sx={{display: "flex",  justifyContent: "center", ml: 2, mr: 2}}>
@@ -29,6 +30,10 @@ export default function RequirementList({
                 </Box>
             ))}
         </List>
+        :
+        <Typography sx={{fontSize: 24, textAlign: "center", fontWeight: "bold", mt: 2}}>
+            No requirement in this document yet
+        </Typography>
     );
 }
 
