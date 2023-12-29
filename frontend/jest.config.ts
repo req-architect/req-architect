e0b3f4f9b7c3dc42d40ffb89adca9b4d96a30b4e
Jan Kowalczewski <official.jan.kk@gmail.com>
2023-12-29 15:52:26 +0100

    Gitlab CI

---
+++ frontend/jest.config.ts
@@ -0,0 +1,8 @@
+export default {
+    preset: "ts-jest",
+    testEnvironment: "jest-environment-jsdom",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+    },
+    reporters: ["default", "jest-junit"],
+};
