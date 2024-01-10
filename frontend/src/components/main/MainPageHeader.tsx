217c8b2a211f94f8d3e9171ee7161bae973beebf
Marcin <mrcin.wawrzyniakk@gmail.com>
2024-01-10 15:41:31 +0100

    added getting commit text from textfield and saving it into vriable

--- frontend/src/components/main/MainPageHeader.tsx
+++ frontend/src/components/main/MainPageHeader.tsx
@@ -7,7 +7,26 @@ import {
     Box,
 } from "@mui/material";
 
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+
+
 export default function MainPageHeader() {
+    const navigate = useNavigate();
+    const [commitTextFieldValue, setcommitTextFieldValue] = useState('');
+
+    const handleLogOut = () => {
+        navigate("/");
+    };
+
+    const changeCommitFieldValue = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setcommitTextFieldValue(event.target.value);
+    }
+    
+    const handleCommit = () => {
+        console.log('Text from TextField:', commitTextFieldValue);
+    };
+
     return (
         <AppBar
             position="static"
@@ -40,6 +59,8 @@ export default function MainPageHeader() {
                         variant="outlined"
                         maxRows={1}
                         sx={{ width: "50%", mr: 2, bgcolor: "white" }}
+                        value={commitTextFieldValue}
+                        onChange={changeCommitFieldValue}
                     ></TextField>
                     <Button
                         sx={{
@@ -52,6 +73,7 @@ export default function MainPageHeader() {
                                 background: "#689F38",
                             },
                         }}
+                        onClick={handleCommit}
                     >
                         SAVE
                     </Button>
@@ -65,6 +87,7 @@ export default function MainPageHeader() {
                         height: "40px",
                         mr: 2,
                     }}
+                    onClick={handleLogOut}
                 >
                     Log out
                 </Button>
