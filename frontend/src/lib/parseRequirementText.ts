afecc0e1577c50d4cc14ef11eb75a6b69bb51a0c
Jan Kowalczewski <official.jan.kk@gmail.com>
2024-01-11 00:10:16 +0100

    parseRequirementText function and tests

---
+++ frontend/src/lib/parseRequirementText.ts
@@ -0,0 +1,56 @@
+type RenderedRequirementComponent = {
+    type: "text" | "uml";
+    text: string;
+};
+
+export class RequirementParseError extends Error {
+    constructor(message: string) {
+        super(message);
+    }
+}
+
+export default function parseRequirementText(text: string) {
+    let buffer: string = "";
+    function pushToBuffer(line: string) {
+        if (buffer.length == 0) {
+            buffer = line;
+        } else {
+            buffer += "\n" + line;
+        }
+    }
+    let inUml: boolean = false;
+    const result: RenderedRequirementComponent[] = [];
+    for (const line of text.split(/\r?\n/)) {
+        if (line.startsWith("@startuml")) {
+            if (inUml) {
+                throw new RequirementParseError(
+                    "Embedding UML diagrams is not supported",
+                );
+            }
+            if (buffer.length > 0) {
+                result.push({ type: "text", text: buffer });
+            }
+            buffer = line;
+            inUml = true;
+        } else if (line.startsWith("@enduml")) {
+            if (!inUml) {
+                throw new RequirementParseError(
+                    "UML diagram end without start",
+                );
+            }
+            pushToBuffer(line);
+            result.push({ type: "uml", text: buffer });
+            buffer = "";
+            inUml = false;
+        } else if (line.length > 0) {
+            pushToBuffer(line);
+        }
+    }
+    if (inUml) {
+        throw new RequirementParseError("UML diagram start without end");
+    }
+    if (buffer.length > 0) {
+        result.push({ type: "text", text: buffer });
+    }
+    return result;
+}
---
+++ frontend/src/test/parseRequirementText.test.ts
@@ -0,0 +1,65 @@
+import { expect, test, describe } from "@jest/globals";
+import parseRequirementText, {
+    RequirementParseError,
+} from "../lib/parseRequirementText.ts";
+
+describe("test parseRequirementText", () => {
+    test("empty string", () => {
+        expect(parseRequirementText("")).toStrictEqual([]);
+    });
+    test("text only", () => {
+        expect(parseRequirementText("hello world")).toStrictEqual([
+            { type: "text", text: "hello world" },
+        ]);
+    });
+    test("paragraph", () => {
+        expect(parseRequirementText("hello\nworld")).toStrictEqual([
+            { type: "text", text: "hello\nworld" },
+        ]);
+    });
+    test("uml only", () => {
+        expect(
+            parseRequirementText('@startuml\ncomponent "Browser"\n@enduml'),
+        ).toStrictEqual([
+            { type: "uml", text: '@startuml\ncomponent "Browser"\n@enduml' },
+        ]);
+    });
+    test("uml and text", () => {
+        expect(
+            parseRequirementText(
+                'hello world\n@startuml\ncomponent "Browser"\n@enduml',
+            ),
+        ).toStrictEqual([
+            { type: "text", text: "hello world" },
+            { type: "uml", text: '@startuml\ncomponent "Browser"\n@enduml' },
+        ]);
+    });
+    test("embedded uml", () => {
+        expect(
+            parseRequirementText(
+                'hello world\n@startuml\ncomponent "Browser"\n@enduml\nhello world',
+            ),
+        ).toStrictEqual([
+            { type: "text", text: "hello world" },
+            { type: "uml", text: '@startuml\ncomponent "Browser"\n@enduml' },
+            { type: "text", text: "hello world" },
+        ]);
+    });
+    test("no UML ending", () => {
+        expect(() =>
+            parseRequirementText("@startuml\ncomponent Browser"),
+        ).toThrow(RequirementParseError);
+    });
+    test("embedded uml in uml", () => {
+        expect(() =>
+            parseRequirementText(
+                '@startuml\ncomponent "Browser"\n@startuml\ncomponent "Hello"\n@enduml\n@enduml',
+            ),
+        ).toThrow(RequirementParseError);
+    });
+    test("uml end without start", () => {
+        expect(() => parseRequirementText("hello\n@enduml")).toThrow(
+            RequirementParseError,
+        );
+    });
+});
