c14e8f21aa375b40aed626763b61c89da278b7f3
Marcin <mrcin.wawrzyniakk@gmail.com>
2024-01-18 15:09:43 +0100

    some integration tests

--- frontend/src/test/integration/integration.test.ts
+++ frontend/src/test/integration/integration.test.ts
@@ -1,8 +1,14 @@
 import { test, describe, beforeEach, afterEach } from "@jest/globals";
 import { GenericContainer, StartedTestContainer } from "testcontainers";
-import { fetchIdentity } from "../../lib/api/authService.ts";
+// import { fetchIdentity } from "../../lib/api/authService.ts";
+import { getRepos, postRepo, postCommit } from "../../lib/api/gitService";
+import { fetchDocuments, postDocument } from "../../lib/api/documentService";
+
+const TEST_TOKEN = "test_token";
+const TEST_REPOS = ["test_repo_1", "test_repo_2"];
 
 let TEST_API_URL = "http://localhost:8000";
+
 
 function constant(constant: string) {
     constant;
@@ -31,7 +37,7 @@ describe("integration tests", () => {
         TEST_API_URL = `http://${container.getHost()}:${container.getMappedPort(
             8000,
         )}`;
-    }, 60000);
+    }, 120000);
 
     afterEach(async () => {
         await container.stop();
@@ -43,8 +49,64 @@ describe("integration tests", () => {
         expect(response.status).toBe(200);
         const text = await response.text();
         expect(text).toBe("OK");
-    }, 60000);
-    test("test", async () => {
-        fetchIdentity("test", new AbortController());
-    });
+    }, 120000);
+
+    test("testGetRepos", async () => {
+        const repos = await getRepos(TEST_TOKEN);
+        expect(repos).toEqual(TEST_REPOS);
+    }, 120000);
+
+    test("testPostRepo", async () => {
+        const response = await postRepo(TEST_TOKEN, TEST_REPOS[0]);
+        expect(response.message).toBe("OK");
+    }, 120000);
+
+    test("testPostCommit", async () => {
+        const response = await postCommit(TEST_TOKEN, TEST_REPOS[0], "Some commit text");
+        expect(response.message).toBe("Successfully staged changes in repository!");
+    }, 120000);
+
+    test("testFetchDocuments", async () => {
+        await postRepo(TEST_TOKEN, TEST_REPOS[0]);
+        const docs = await fetchDocuments(TEST_TOKEN, TEST_REPOS[0]);
+        expect(docs).toEqual([]);
+    }, 120000);
+
+    test("testPostDocument", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        const docs = await fetchDocuments(TEST_TOKEN, repo);
+        expect(docs.length).toBe(1);
+        expect(docs[0].prefix).toBe("root");
+        expect(docs[0].children).toEqual([]);
+    }, 120000);
+
+    // test("testPostDocumentWithParent", async () => {
+    //     const repo = TEST_REPOS[0];
+    //     await postRepo(TEST_TOKEN, repo);
+    //     await postDocument(TEST_TOKEN, repo, "root");
+    //     await postDocument(TEST_TOKEN, repo, "SYSREQ", "root");
+    //     const docs = await fetchDocuments(TEST_TOKEN, repo);
+
+    //     expect(docs.length).toBe(1);
+    //     expect(docs[0].prefix).toBe("root");
+    //     const children = docs[0].children;
+    //     expect(children).toBeDefined();
+    //     if (children) {
+    //         expect(children.length).toBe(1);
+    //         expect(children[0].prefix).toBe("SYSREQ");
+    //         // expect(children[0].children).toBeUndefined();
+    //     }
+    // }, 120000);
+
+    // test("testDeleteDocument", async () => {
+    //     const repo = TEST_REPOS[0];
+    //     await postRepo(TEST_TOKEN, repo);
+    //     await postDocument(TEST_TOKEN, repo, "root");
+    //     const docs = await fetchDocuments(TEST_TOKEN, repo);
+    //     expect(docs.length).toBe(1);
+    //     expect(docs[0].prefix).toBe("root");
+    //     expect(docs[0].children).toEqual([]);
+    // }, 120000);
 });
