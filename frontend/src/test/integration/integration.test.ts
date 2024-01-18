c34d89f0ac973f4830831c2f94a48db0668e2d22
Marcin <mrcin.wawrzyniakk@gmail.com>
2024-01-18 22:05:23 +0100

    requirementService tests

--- frontend/src/test/integration/integration.test.ts
+++ frontend/src/test/integration/integration.test.ts
@@ -3,7 +3,7 @@ import { GenericContainer, StartedTestContainer } from "testcontainers";
 // import { fetchIdentity } from "../../lib/api/authService.ts";
 import { getRepos, postRepo, postCommit } from "../../lib/api/gitService";
 import { fetchDocuments, postDocument, deleteDocument } from "../../lib/api/documentService";
-import { fetchRequirements, postRequirement } from "../../lib/api/requirementService";
+import { fetchRequirements, postRequirement, deleteRequirement, linkRequirement, unlinkRequirement, putRequirement, getAllRequirements } from "../../lib/api/requirementService";
 
 const TEST_TOKEN = "test_token";
 const TEST_REPOS = ["test_repo_1", "test_repo_2"];
@@ -119,7 +119,29 @@ describe("integration tests", () => {
         expect(reqs).toEqual([]);
     }, 120000);
 
-    test("testAddRequirements", async () => {
+    test("testGetAllRequirements", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postDocument(TEST_TOKEN, repo, "SYSREQ", "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "SYSREQ");
+
+        const reqs = await getAllRequirements(TEST_TOKEN, repo);
+        expect(reqs.length).toBe(3);
+        const root001 = reqs[0];
+        const root002 = reqs[1];
+        const SYSREQ001 = reqs[2];
+        expect(root001.id).toBe("root001");
+        expect(root001.docPrefix).toBe("root"); 
+        expect(root002.id).toBe("root002");
+        expect(root002.docPrefix).toBe("root");
+        expect(SYSREQ001.id).toBe("SYSREQ001");
+        expect(SYSREQ001.docPrefix).toBe("SYSREQ");
+    }, 120000);
+
+    test("testAddRequirement", async () => {
         const repo = TEST_REPOS[0];
         await postRepo(TEST_TOKEN, repo);
         await postDocument(TEST_TOKEN, repo, "root");
@@ -132,5 +154,72 @@ describe("integration tests", () => {
         expect(req.text).toBe("");
         expect(req.links).toEqual([]);
         expect(req.reviewed).toBe(false);
+    }, 120000);
+
+    test("testDeleteRequirement", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await deleteRequirement(TEST_TOKEN, repo, "root001");
+
+        const reqs = await fetchRequirements(TEST_TOKEN, repo, "root");
+        expect(reqs.length).toBe(0);
+    }, 120000);
+
+    test("testDeleteRequirement-MultipleReqsInRepo", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await deleteRequirement(TEST_TOKEN, repo, "root001");
+
+        const reqs = await fetchRequirements(TEST_TOKEN, repo, "root");
+        expect(reqs.length).toBe(1);
+        const req = reqs[0];
+        expect(req.id).toBe("root002");
+    }, 120000);
+    
+    test("testPutRequirement", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await putRequirement(TEST_TOKEN, repo, "root001", "Some requirement text");
+
+        const reqs = await fetchRequirements(TEST_TOKEN, repo, "root");
+        expect(reqs[0].text).toBe("Some requirement text");
+    }, 120000);
+
+    test("testLinkRequirement", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await linkRequirement(TEST_TOKEN, repo, "root001", "root002");
+
+        const reqs = await fetchRequirements(TEST_TOKEN, repo, "root");
+        const root001 = reqs[0];
+        const root002 = reqs[1];
+        expect(root001.links).toEqual(["root002"]);
+        expect(root002.links).toEqual([]);
+    }, 120000);
+
+    test("testUnlinkRequirement", async () => {
+        const repo = TEST_REPOS[0];
+        await postRepo(TEST_TOKEN, repo);
+        await postDocument(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await postRequirement(TEST_TOKEN, repo, "root");
+        await linkRequirement(TEST_TOKEN, repo, "root001", "root002");
+        await unlinkRequirement(TEST_TOKEN, repo, "root001", "root002");
+
+        const reqs = await fetchRequirements(TEST_TOKEN, repo, "root");
+        const root001 = reqs[0];
+        const root002 = reqs[1];
+        expect(root001.links).toEqual([]);
+        expect(root002.links).toEqual([]);
     }, 120000);
 });
